2537b0be4c6b7e14102b0be21f71d09d91e9a3a9
Steve Jarvis <sajarvis@bu.edu>
2016-03-21 22:37:25 -0400

    Basic distributed deploy and callback working.
    Run server, it deploys client to configured IP. Client contacts
    the REST API after deploy. Hacky, on our way to a proof of concept
    hopefully.

--- src/client.rb
+++ src/client.rb
@@ -1,13 +1,31 @@
+# Application to run on all nodes to determine connectivity.
+
 require 'net/http'
 
 port = 4567
-host = '127.0.0.1'
+host = '192.168.0.27'
 
-puts "Reporting data to server process..."
+# Helper to determine if the server is available yet.
+def serverup?(ip, port)
+  http = Net::HTTP.start(ip, port, {open_timeout:3, read_timeout:3})
+  response = http.head('/')
+  puts response.code
+  response.code == '200'
+rescue Timeout::Error, SocketError, Errno::ECONNREFUSED
+  false
+end
+
+# Wait for server to come up.
+until serverup?(host, port)
+  puts "Waiting to rock until REST API is available"
+  sleep(1)
+end
+
+puts "Reporting data to server process"
 http = Net::HTTP.new(host, port)
 response = http.send_request('PUT', '/me?visible=192.168.0.1', 'body')
 puts response.code
 
-puts "Now requesting that info back..."
+puts "Now requesting that info back"
 response = http.send_request('GET', '/me')
 puts "#{response.code} --> #{response.body}"
--- src/server.rb
+++ src/server.rb
@@ -1,17 +1,53 @@
-require "sinatra"
+# Application to execute on the console node. Offers a RESTful interface for
+# modifying the network map and finds the desired topology.
+#
+# Run from root of application dir.
 
+require 'sinatra'
+require 'net/ssh'
+require 'net/scp'
+require 'net/http'
+
+set :bind, '0.0.0.0'
+
+# These ssh keys must be added to the agent, net-ssh does not have its own
+# implementation of an agent.
+nodes = [{'ip':'192.168.0.101', 'user':'pi', 'key':'/home/sjarvis/.ssh/id_rsa'}]
+
+# Deploy and execute the client to gather information on the
+# nodes.
+Thread.new {
+  nodes.each do |node|
+    dst_path = '/tmp/'
+    puts "Deploying to #{node[:ip]}:#{dst_path}"
+    Net::SCP.upload!(node[:ip], node[:user],
+                     "#{Dir.pwd}/src/client.rb", dst_path,
+                     :ssh => {:keys => [node[:key]]})
+    Net::SSH.start(node[:ip], node[:user], :keys => [node[:key]]) do |ssh|
+      puts ssh.exec! "ruby #{dst_path}/client.rb"
+    end
+  end
+}
+
+# House the set of information regarding connectivity.
 data = {}
 
-get '/:arg' do
-  # Return the requested info.
-  puts "Getting #{params['arg']}..."
-  # Not actually what we want to do but that's ok
-  return data["#{params['arg']}"]
+# Simple GET to ping root URL.
+get '/' do
+  "Hola"
 end
 
+# GET requests a particular topology, which we compute based on the current
+# known network graph.
+get '/:arg' do
+  puts "Getting #{params['arg']}"
+  data["#{params['arg']}"]
+end
+
+# PUT saves the supplied mapping information. The sender should include
+# a list of all nodes visible to it, stored by IP. Or ID? I don't know.
 put '/:sender' do
-  # Save the supplied mapping information.
-  puts "Putting for #{params['sender']} = #{:visible}..."
+  puts "Putting for #{params['sender']} = #{:visible}"
   data["#{params['sender']}"] = params[:visible]
-  return 'ok'
+  "Ok"
 end
