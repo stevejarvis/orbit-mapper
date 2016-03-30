2d53410c8dd2810d8016ffc37b643b8bb2140cdd
Steve Jarvis <sajarvis@bu.edu>
2016-03-29 20:06:07 -0400

    updated experiment to get closer to running in Orbit

--- example/map_experiment.rb
+++ example/map_experiment.rb
@@ -1,10 +1,13 @@
-# This file is based on an automatically generated version by
-# oml2-scaffold 2.12.0pre.147-8fe8-dirty
-# The syntax of this file is documented at [0].
+# The syntax of this file is documented at [0] and [1].
 #
 # [0] http://doc.mytestbed.net/doc/omf/OmfEc/Backward/AppDefinition.html
+# [1] http://omf.mytestbed.net
 
-defApplication('sjarvis:app:map_experiment', 'map_experiment') do |app|
+name = "Mapper Experiment"
+baseTopo = Topology['system:topo:imaged']
+defProperty('duration', 60, "Duration of the experiment")
+
+defApplication('map_experiment', 'map_experiment') do |app|
 
   app.version(1, 0, 0)
   app.shortDescription = 'Experiment to Demonstrate Using Mapping API'
@@ -13,7 +16,9 @@ This is an experiment to demonstrate how an existing experiment can use the
 mapping API provided by Orbit Mapper. There are no additional measurements
 needed, those taken here are only for demonstration.
 }
-  app.path = "/usr/local/bin/map_experiment"
+  app.path = "/experiment/map_experiment"
+  # Make this tarball actually exist: tar -cf experiment.tar map_exeriment
+  app.appPackage = "experiment.tar"
 
   # Declare command-line arguments; generate Popt parser with
   #  oml2-scaffold --opts map_experiment.rb
@@ -28,7 +33,33 @@ needed, those taken here are only for demonstration.
     mp.defMetric('self', :int32)
     mp.defMetric('topology', :vector_int32)
   end
+end
 
+defTopology('nodes') do |t|
+  t.addNode(baseTopo.getNodeByIndex(0))
+  t.addNode(baseTopo.getNodeByIndex(1))
+end
+
+defGroup('ExperimentNodes', 'nodes') do |node|
+  node.addApplication("map_experiment") do |app|
+    app.measure('query', :samples => 1)
+  end
+  node.net.w1.mode = "adhoc"
+  node.net.w1.type = 'g'
+  node.net.w1.channel = "6"
+  node.net.w1.essid = "helloworld"
+  node.net.w1.ip = "192.168.0.2"
+end
+
+onEvent(:ALL_UP_AND_INSTALLED) do |event|
+  info "Running experiment..."
+  wait 10
+  allGroups.startApplications
+  info "All applications are started..."
+  wait property.duration
+  allGroups.stopApplications
+  info "All my Applications are stopped."
+  Experiment.done
 end
 
 # Local Variables:
